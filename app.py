c3954bfd377df9af73bf3a3a32353cc1333fbdd4
jimmmmothy <sencor0442@gmail.com>
2022-10-20 15:25:01 +0200

    Finished marking orders as ready

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ def pay():
                 orderData.append(item)
 
     # writing the order in a csv file
-    write_order(orderData)
+    append_file("pending_orders.csv", orderData)
 
     return redirect('/complete')
 
@@ -38,36 +38,43 @@ def complete():
 
 @app.route('/staff')
 def staff():
-    order = read_order()
+    pendingOrders = read_file("pending_orders.csv")
+    readyOrders = read_file("ready_orders.csv")
 
-    return render_template('luigi.html', order=order)
+    return render_template('luigi.html', pendingOrders=pendingOrders, readyOrders=readyOrders)
 
-@app.route('/order-ready')
+@app.route('/order-ready', methods=["POST"])
 def order_ready():
-    order = read_order()
+    item = request.json
+    itemAsList = [item["tableNumber"], item["item"]]
 
+    order = read_file("pending_orders.csv")
 
-    
+    order.remove(itemAsList)
+    update_file("pending_orders.csv", order)
+    append_file("ready_orders.csv", [itemAsList])
 
-def write_order(order):
-    with open('orders.csv', 'a', newline='') as f:
+    return redirect('/staff')
+
+def append_file(file, order):
+    with open(file, 'a', newline='') as f:
         writer = csv.writer(f)
         writer.writerows(order)
-
+    
     return "File has been written"
 
-def read_order():
+def read_file(file):
     order = []
 
-    with open("orders.csv", 'r') as filevar :
+    with open(file, 'r') as filevar :
         reader = csv.reader(filevar)
         for row in reader :
             order.append(row)
 
     return order
 
-def update_file(updatedList):
-    with open("fakefacebook.txt","w",newline="") as f:
+def update_file(file, updatedList):
+    with open(file,"w",newline="") as f:
         Writer=csv.writer(f)
         Writer.writerows(updatedList)
 
